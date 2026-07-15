15e85311b5e06119f3a7d9366c1a7cf6a6b76960
Victor <rotcivbrito2003@gmal.com>
2026-07-15 18:27:41 -0300

    subido server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = require("./app")
+
+const sequelize = require("./config/database")
+
+const PORT = 3001
+
+app.liste(PORT, () =>{
+    console.log("Microserviço rodando na porta 3001")
+})
